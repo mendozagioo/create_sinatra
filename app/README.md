761f857303cf18f6677c1e08494aaa899de8b32a
Giovanni Mendoza <mendozagioo@gmail.com>
2014-04-03 22:32:11 -0600

    Complete repo added to copy all files

---
+++ app/README.md
@@ -0,0 +1,7 @@
+# A simple Sinatra app
+
+Please run bundle before to start:
+    $ bundle install
+
+Run the server with:
+    $ shotgun
--- create_sinatra.rb
+++ create_sinatra.rb
@@ -24,205 +24,33 @@ class CreateSinatra
 
     FileUtils.mkdir_p "../#{@name_app}/views"
 
-    create_file
+    copy_files
   end
 
-  def self.create_file
-    file_app = File.new "../#{@name_app}/app.rb", "w"
-    file_config = File.new "../#{@name_app}/config.ru", "w"
-    file_gemfile = File.new "../#{@name_app}/Gemfile", "w"
-    file_rakefile = File.new "../#{@name_app}/Rakefile", "w"
-    file_readme = File.new "../#{@name_app}/README.md", "w"
-    file_applicationjs = File.new "../#{@name_app}/assets/javascripts/application.js", "w"
-    file_applicationsass = File.new "../#{@name_app}/assets/stylesheets/application.sass", "w"
-    file_local_en = File.new "../#{@name_app}/config/locales/en.yml", "w"
-    file_local_es = File.new "../#{@name_app}/config/locales/es.yml", "w"
-    file_layout_view = File.new "../#{@name_app}/views/layout.haml", "w"
-    file_index_view = File.new "../#{@name_app}/views/index.haml", "w"
-
-    File.new "../#{@name_app}/assets/stylesheets/config/_responsive-large.sass", "w"
-    File.new "../#{@name_app}/assets/stylesheets/config/_responsive-medium.sass", "w"
-    File.new "../#{@name_app}/assets/stylesheets/config/_responsive-short.sass", "w"
-    File.new "../#{@name_app}/assets/stylesheets/config/_variables.sass", "w"
-    File.new "../#{@name_app}/assets/stylesheets/ui/_base.sass", "w"
-    File.new "../#{@name_app}/assets/stylesheets/ui/_buttons.sass", "w"
-    File.new "../#{@name_app}/assets/stylesheets/ui/_forms.sass", "w"
-    File.new "../#{@name_app}/assets/stylesheets/ui/_icons.sass", "w"
-    File.new "../#{@name_app}/assets/stylesheets/ui/_images.sass", "w"
-    File.new "../#{@name_app}/assets/stylesheets/ui/_lists.sass", "w"
-    File.new "../#{@name_app}/assets/stylesheets/ui/_tables.sass", "w"
-    File.new "../#{@name_app}/assets/stylesheets/ui/_texts.sass", "w"
-
-    create_app_file(file_app)
-    create_config_file(file_config)
-    create_gemfile_file(file_gemfile)
-    create_rakefile_file(file_rakefile)
-    create_readme_file(file_readme)
-    create_applicationjs_file(file_applicationjs)
-    create_applicationsass_file(file_applicationsass)
-    create_local_en_file(file_local_en)
-    create_local_es_file(file_local_es)
-    create_layout_view_file(file_layout_view)
-    create_index_view_file(file_index_view)
-  end
-
-  def self.create_app_file(file)
-    file.puts "# Base file
-require \'bundler\'
-
-Bundler.require
-
-require \'sinatra/i18n\'
-require \'sinatra/partial\'
-
-class App < Sinatra::Base
-  register Sinatra::AssetPipeline
-  register Sinatra::I18n
-
-  set :root, File.dirname(__FILE__)
-  set :assets_precompile, %w(application.js application.css *.png *.jpg *.svg *.ico *.otf *.eot *ttf)
-
-  configure do
-    sprockets.append_path File.join(root, \'assets\', \'stylesheets\')
-    sprockets.append_path File.join(root, \'assets\', \'javascripts\')
-    sprockets.append_path File.join(root, \'assets\', \'images\')
-    sprockets.append_path File.join(root, \'assets\', \'fonts\')
-    sprockets.append_path File.join(root, \'vendor\', \'javascripts\')
-
-    I18n.enforce_available_locales = false
-    I18n.default_locale = :en
-  end
-
-  configure :production do
-    set :raise_errors, false
-    set :show_exceptions, false
-  end
-
-  get \'/\' do
-    haml :index
-  end
-end"
-    file.close
-  end
-
-  def self.create_config_file(file)
-    file.puts "# This file is used by Rack-based servers to start the app.
-
-require File.join(File.dirname(__FILE__), \'app.rb\')
-
-run App.new"
-    file.close
-  end
-
-  def self.create_gemfile_file(file)
-    file.puts "# Gemfile
-source \'https://rubygems.org\'
-
-ruby \'2.1.1\'
-
-gem \'sinatra\'
-gem \'haml\'
-gem \'i18n\'
-
-gem \'sinatra-asset-pipeline\', require: \'sinatra/asset_pipeline\'
-gem \'sinatra-i18n\', require: \'sinatra/i18n\'
-
-gem \'shotgun\'
-
-gem \'cactu\'
-
-gem \'sinatra-partial\'"
-    file.close
-  end
-
-  def self.create_rakefile_file(file)
-    file.puts "# Rakefile
-require \'sinatra/asset_pipeline/task.rb\'
-require \'./app\'
-
-Sinatra::AssetPipeline::Task.define! App"
-    file.close
-  end
-
-  def self.create_readme_file(file)
-    file.puts "# A simple Sinatra app
-
-Please run bundle before to start:
-    $ bundle install
-
-Run the server with:
-    $ shotgun"
-    file.close
-  end
-
-  def self.create_applicationjs_file(file)
-    file.puts "// application.js
-//= require_tree ."
-    file.close
-  end
-
-  def self.create_applicationsass_file(file)
-    file.puts "@import \'cactu\'
-
-@import \'config/variables\'
-@import \'config/responsive-large\'
-@import \'config/responsive-medium\'
-@import \'config/responsive-short\'
-
-@import \'ui/base\'
-@import \'ui/texts\'
-@import \'ui/buttons\'
-@import \'ui/icons\'
-@import \'ui/tables\'
-@import \'ui/forms\'
-@import \'ui/images\'"
-    file.close
-  end
-
-  def self.create_local_en_file(file)
-    file.puts "en:
-  title: \'Hello app\'"
-    file.close
-  end
-
-  def self.create_local_es_file(file)
-    file.puts "es:
-  title: \'Hola app\'"
-    file.close
-  end
-
-  def self.create_layout_view_file(file)
-    stylesheet_path = '"#{stylesheet_path \'application\'}"'
-    javascript_path = '"#{javascript_path \'application\'}"'
-    image_path = '"#{image_path \'favicon.ico\'}"'
-
-    file.puts "!!!
-%html{lang: \"en\"}
-  %head
-    %meta{charset: \"utf-8\"}
-    %meta{name: \'viewport\', content: \'width=device-width, initial-scale=1.0 maximum-scale=1.0\'}
-    %title= t(\'title\')
-    %meta{name: \"author\", content: \"Giovanni Mendoza\"}
-    %meta{content: \'TITLE HERE\', property: \'og:title\'}
-    %meta{content: \'URL HERE\', property: \'og:url\'}
-    %meta{content: \'DESCRIPTION HERE\', property: \'og:description\'}
-    %meta{content: \'NAME DESCRIPTION HERE\', name: \'description\'}
-    %meta{name: \"keywords\", content: \"KEYWORDS HERE\"}
-    %link{rel: \'stylesheet\', href: \"http://fonts.googleapis.com/css?family=Oswald:400,300,700&subset=latin\"}
-    %link{rel: \'stylesheet\', href: #{stylesheet_path} }
-    %link{rel: \"shortcut icon\", type: \"image/vnd.microsoft.icon\", href: #{image_path} }
-  %body
-
-    = yield
-
-    %script{type: \"text/javascript\", charset: \"utf-8\", src: \"http://code.jquery.com/jquery-1.10.2.min.js\"}
-    %script{src: #{javascript_path}}"
-    file.close
-  end
-
-  def self.create_index_view_file(file)
-    file.puts "%p= t(\'title\')"
-    file.close
+  def self.copy_files
+    FileUtils.cp "app/assets/javascripts/application.js", "../#{@name_app}/assets/javascripts/application.js"
+    FileUtils.cp "app/assets/stylesheets/config/_responsive-large.sass", "../#{@name_app}/assets/stylesheets/config/_responsive-large.sass"
+    FileUtils.cp "app/assets/stylesheets/config/_responsive-large.sass", "../#{@name_app}/assets/stylesheets/config/_responsive-medium.sass"
+    FileUtils.cp "app/assets/stylesheets/config/_responsive-large.sass", "../#{@name_app}/assets/stylesheets/config/_responsive-short.sass"
+    FileUtils.cp "app/assets/stylesheets/config/_responsive-large.sass", "../#{@name_app}/assets/stylesheets/config/_variables.sass"
+    FileUtils.cp "app/assets/stylesheets/ui/_base.sass", "../#{@name_app}/assets/stylesheets/ui/_base.sass"
+    FileUtils.cp "app/assets/stylesheets/ui/_buttons.sass", "../#{@name_app}/assets/stylesheets/ui/_buttons.sass"
+    FileUtils.cp "app/assets/stylesheets/ui/_forms.sass", "../#{@name_app}/assets/stylesheets/ui/_forms.sass"
+    FileUtils.cp "app/assets/stylesheets/ui/_icons.sass", "../#{@name_app}/assets/stylesheets/ui/_icons.sass"
+    FileUtils.cp "app/assets/stylesheets/ui/_images.sass", "../#{@name_app}/assets/stylesheets/ui/_images.sass"
+    FileUtils.cp "app/assets/stylesheets/ui/_lists.sass", "../#{@name_app}/assets/stylesheets/ui/_lists.sass"
+    FileUtils.cp "app/assets/stylesheets/ui/_tables.sass", "../#{@name_app}/assets/stylesheets/ui/_tables.sass"
+    FileUtils.cp "app/assets/stylesheets/ui/_texts.sass", "../#{@name_app}/assets/stylesheets/ui/_texts.sass"
+    FileUtils.cp "app/assets/stylesheets/application.sass", "../#{@name_app}/assets/stylesheets/application.sass"
+    FileUtils.cp "app/config/locales/en.yml", "../#{@name_app}/config/locales/en.yml"
+    FileUtils.cp "app/config/locales/es.yml", "../#{@name_app}/config/locales/es.yml"
+    FileUtils.cp "app/views/index.haml", "../#{@name_app}/views/index.haml"
+    FileUtils.cp "app/views/layout.haml", "../#{@name_app}/views/layout.haml"
+    FileUtils.cp "app/app.rb", "../#{@name_app}/app.rb"
+    FileUtils.cp "app/config.ru", "../#{@name_app}/config.ru"
+    FileUtils.cp "app/Gemfile", "../#{@name_app}/Gemfile"
+    FileUtils.cp "app/Rakefile", "../#{@name_app}/Rakefile"
+    FileUtils.cp "app/README.md", "../#{@name_app}/README.md"
   end
 end
 
